edac9e0c38856f337652ea553284e4a7ab8ca2d4
IsaacSemb <isaacsemb1996@gmail.com>
2025-01-09 02:00:46 +0000

    practiced on contacting a backend, then factored out api client so that the logic responsible for builsing a URL is separated by building a baseURL then joining endpoints to it. next is to do separations of concerns by removing all http related stuff to a separate module

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 // import AppExpenseTracker0 from "./expense-tracker-v0/AppExpenseTracker0";
-import AppExpenseTracker1 from "./expense-tracker-v1/AppExpenseTracker1";
+import App1_async from "./0000-practice/fetching backend-data/App1_async";
+import App1_promise from "./0000-practice/fetching backend-data/App1_promise";
+// import AppExpenseTracker1 from "./expense-tracker-v1/AppExpenseTracker1";
 
 function App() {
   return (
     <>
       {/* <AppExpenseTracker0 /> */}
-      <AppExpenseTracker1 />
+      {/* <AppExpenseTracker1 /> */}
+      <App1_promise/>
     </>
   );
 }
